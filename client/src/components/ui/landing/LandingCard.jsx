88dac8725e53347abebb9f33dd8d82d0550f73d4
Ashley Bhandari <99090595+ashleybhandari@users.noreply.github.com>
2025-03-22 20:50:00 -0400

    Add landing page

---
+++ client/src/components/ui/landing/LandingCard.jsx
@@ -0,0 +1,11 @@
+/**
+ * @param {React.ReactNode} children - Text to display
+ * @returns Box in secondary color that displays text
+ */
+export default function LandingCard({ children }) {
+  return (
+    <div className="flex items-center justify-center md:w-56 lg:w-72 h-52 p-12 text-2xl font-bold rounded-lg uppercase bg-secondary text-on-secondary">
+      {children}
+    </div>
+  );
+}
--- client/src/views/Landing.jsx
+++ client/src/views/Landing.jsx
@@ -1,3 +1,29 @@
+import { Button } from "@/components/ui/button";
+import LandingCard from "@/components/ui/landing/LandingCard";
+
+/**
+ * @returns Landing page
+ */
 export default function Landing() {
-    return <p>landing</p>
+  return (
+    <div>
+      <div className="flex flex-col justify-end gap-7 h-72 p-10 bg-primary">
+        <h1 className="font-bold text-6xl text-on-primary">trip planner</h1>
+        <Button variant="outline" className="w-fit border-none text-primary">
+          get started
+        </Button>
+      </div>
+      <div className="flex flex-col items-center gap-10 p-10">
+        <div className="flex flex-col md:flex-row gap-8">
+          <LandingCard>Everyone's daily itinerary</LandingCard>
+          <LandingCard>Everyone's maps and pins</LandingCard>
+          <LandingCard>Everyone's budget and expenses</LandingCard>
+        </div>
+        <div className="text-3xl text-center font-bold text-secondary">
+          all in just <span className="underline">one place</span>
+        </div>
+        <Button className="w-fit text-on-primary">get started</Button>
+      </div>
+    </div>
+  );
 }
