68f1b77734d679212642a7e1f3d8dee2a31d0fc7
Ashley Bhandari <99090595+ashleybhandari@users.noreply.github.com>
2025-03-28 20:36:00 -0400

    Adjust sidebar width

--- client/src/components/layout/sidebar/Sidebar.tsx
+++ client/src/components/layout/sidebar/Sidebar.tsx
@@ -3,7 +3,7 @@ import { SidebarPages } from "./SidebarPages";
 
 export function Sidebar() {
   return (
-    <div className="h-screen flex flex-col p-2 w-52 bg-secondary">
+    <div className="h-screen flex flex-col p-2 w-60 bg-secondary">
       <SidebarPages />
       <SidebarFooter />
     </div>
