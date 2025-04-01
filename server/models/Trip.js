6cdb534f93ee9bd892e757de3c8cb7483765a2c6
aishwarya-vr04 <200049374+aishwarya-vr04@users.noreply.github.com>
2025-04-01 05:48:52 -0400

    Add Trip model to server

---
+++ server/models/Trip.js
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose';
+import User from './User.js';
+import {Checklist} from './Checklist.js'
+const { Schema } = mongoose;
+
+const tripSchema= new Schema({
+  tripName:{
+    type: String,
+    required: true,
+  }, 
+  destinations:
+    {
+        type: String, 
+        required: true
+    }, 
+  tripSlug:
+  {
+        type: String, 
+        required: true, 
+        unique: true,
+  }, 
+  users:
+  [{
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: User
+  }
+
+  ], 
+  checklists:
+  [
+    {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: Checklist, 
+        required: false
+    }
+  ]
+},
+  {timestamps: true}
+);
+
+
+export default mongoose.model("Trip", tripSchema);
