3003506d3be7980bf4a65e1ceb74fd834325ecbd
aishwarya-vr04 <200049374+aishwarya-vr04@users.noreply.github.com>
2025-05-04 14:13:27 -0400

    Add socket controller

---
+++ server/sockets/socket.js
@@ -0,0 +1,32 @@
+import { Server } from "socket.io";
+
+let ioInstance;
+
+export const initSocket = (server) => {
+  ioInstance = new Server(server, {
+    cors: {
+      origin: "http://localhost:5173",
+      credentials: true,
+    },
+  });
+
+  ioInstance.on("connection", (socket) => {
+    console.log("Client connected:", socket.id);
+
+    socket.on("join-trip", (tripId) => {
+      console.log(`Joining trip room: ${tripId}`);
+      socket.join(tripId);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("Client disconnected:", socket.id);
+    });
+  });
+
+  return ioInstance;
+};
+
+export const getIO = () => {
+  if (!ioInstance) throw new Error("Socket.io not initialized!");
+  return ioInstance;
+};
